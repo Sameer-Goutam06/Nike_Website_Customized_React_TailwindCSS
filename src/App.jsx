7e0199ea070f4f69e77a667f1384e509a22b4ebf
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2025-01-10 17:20:00 +0530

    Setup and structuring completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+/* eslint-disable no-unused-vars */
+import React from 'react'
+// import Main from './components/main'//no issues with file casing
+export default function App(){
+  return(
+    <main>  
+        Nav
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className="padding">  
+        Popular Products        
+      </section>
+      <section className="padding">  
+        Super Quality    
+      </section>
+      <section className="padding">  
+        Services     
+      </section>
+      <section className="padding">  
+        Customer Reviews      
+      </section> 
+      <section className="padding">  
+        Subscribe       
+      </section>
+      <section className="padding">  
+        Footer       
+      </section>
+    </main>
   )
 }
 
-export default App
+//main refers to main.jsx
