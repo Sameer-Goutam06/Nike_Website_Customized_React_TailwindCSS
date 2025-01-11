dfe4d06a5797244ba1be57f3dc3f13d54cf29d23
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2025-01-11 13:34:19 +0530

    added sign in to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,11 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
+            <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+              <a href='/'>Sign in</a>
+              <span>/</span>
+              <a href='/'>Explore now</a>
+            </div>
             <div className='hidden max-lg:block'>
               <img src={hamburger} alt="Ham" width={25} height={25}></img>
             </div>
