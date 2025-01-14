0e902a083809d809b1e00806162c46219b1fddca
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2025-01-14 07:38:55 +0530

    Changed File names for extra code issues and completed the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import {Hero,PopularProducts,SuperQuality,SpecialOffers,Services,CustomerReviews,Subscribe,Footer} from "./sections/SectionsImporter"
-import Navbar from './components/Navbar'
-export default function App(){
-  return(
-    <main>  
-        <Navbar/>
-      <section className='xl:padding-1 wide:padding-r padding-b'>
-        <Hero/>
+import { Nav } from "./components";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
       </section>
-      <section className="padding">  
-        <PopularProducts/>        
+      <section className='padding'>
+        <PopularProducts />
       </section>
-      <section className="padding">  
-        <SuperQuality/>    
+      <section className='padding'>
+        <SuperQuality />
       </section>
-      <section className="padding">  
-        <Services/>     
+      <section className='padding-x py-10'>
+        <Services />
       </section>
-      <section className="padding">  
-        <CustomerReviews/>      
-      </section> 
-      <section className="padding">  
-        <Subscribe/>       
+      <section className='padding'>
+        <SpecialOffer />
       </section>
-      <section className="padding">  
-        <Footer/>       
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-//main refers to main.jsx
+export default App;
